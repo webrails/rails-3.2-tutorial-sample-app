41350317f19f7c4e133fa7b3d21663aa8c77fdea
Marc Bungart <dev@webrails.de>
2012-10-20 22:19:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f279119feca798d86b13551a565fc114a473a9f5bc2295517a14b449d6a9c688334512321dd73647dcabf8f81afe17738b4b741cfa17640f8be91effd43578ba'
